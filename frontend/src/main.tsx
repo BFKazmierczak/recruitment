468aaab12efb49863167a7e1969ef7ce90ecd08b
BFKazmierczak <bfkazmierczak@gmail.com>
2024-07-23 22:22:59 +0200

    feat(frontend/styling): add custom mui theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,29 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from './App';
+import { store } from '@/src/state/store';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import Layout from './components/layout/Layout';
 import ErrorPage from './error-page';
-import Root from './routes/root';
+import { rootLoader } from './loaders';
+import Add from './routes/Add';
+import Messages from './routes/Messages';
+import Root from './routes/Root';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ffd20c',
+      contrastText: '#000',
+    },
+    secondary: {
+      main: '#000',
+    },
+  },
+});
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -26,6 +44,10 @@ const router = createBrowserRouter([
 
 root.render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </ThemeProvider>
   </StrictMode>,
 );
