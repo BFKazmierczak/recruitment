869d2ceb212b06ee9ac5ae2c8de2f02b3b7b422a
BFKazmierczak <bfkazmierczak@gmail.com>
2024-07-24 15:14:11 +0200

    feat(frontend/data): add createPost function

--- frontend/src/mutations/index.ts
+++ frontend/src/mutations/index.ts
@@ -1,4 +1,35 @@
-export function updatePost(id: number, content: string) {
+import { PostType } from '../shared/types';
+
+type GenericResponse<T> = { status: 0; payload: T } | { status: 1; error: string };
+
+export async function createPost(content: string): Promise<GenericResponse<PostType>> {
+  const response = await fetch('http://localhost:3000/api/posts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      author: 'bfkazmierczak@gmail.com',
+      content,
+      createdAt: Date.now(),
+    }),
+  });
+
+  if (!response.ok)
+    return {
+      status: 1,
+      error: response.statusText,
+    };
+
+  const post = await response.json();
+
+  return {
+    status: 0,
+    payload: post,
+  };
+}
+
+export async function updatePost(id: number, content: string) {
   fetch('http://localhost:3000/api/posts', {
     method: 'PUT',
     body: JSON.stringify({
@@ -7,8 +38,10 @@ export function updatePost(id: number, content: string) {
   });
 }
 
-export function deletePost(id: number) {
-  fetch(`http://localhost:3000/api/posts/${id}`, {
+export async function deletePost(id: number) {
+  await fetch(`http://localhost:3000/api/posts/${id}`, {
     method: 'DELETE',
   });
+
+  return true;
 }
